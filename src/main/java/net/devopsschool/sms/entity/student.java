d6a90c2555dd88ab1fdcede588af3ab823f1be52
kund <kund.jha@gmail.com>
2021-11-21 19:05:10 +0530

    this is to commit

---
+++ src/main/java/net/devopsschool/sms/entity/student.java
@@ -0,0 +1,10 @@
+package net.devopsschool.sms.entity;
+
+#student
+public class student {
+
+      private long id;
+      private String firstname;
+      private String lastname;
+      private String email;
+}
---
+++ src/test/java/net/devopsschool/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.devopsschool.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
